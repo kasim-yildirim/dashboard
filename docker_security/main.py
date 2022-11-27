85291c6b08961065032e797056fbcc65e38d59c7
Kasım Yıldırım <64635148+kasim-yildirim@users.noreply.github.com>
2022-11-27 16:20:57 +0300

    Add files via upload

---
+++ docker_security/main.py
@@ -0,0 +1,15 @@
+import os
+
+
+# history | cut -c 8- | tail
+# os.system("sudo su")
+# input("emreali")
+# os.system("history | cut -c 8- ")
+os.system("history > logs_command.txt")
+os.system("history > logs_command.txt")
+#     print(i +"-----------*")
+
+# print(len(dataa_Log))
+
+# if dataa_Log in "docker ":
+#     print("e")
